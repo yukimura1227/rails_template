78d81094defba497946cfabf414e9c23e73db76e
yukimura1227 <takamura1227@gmail.com>
2017-04-23 12:04:01 +0000

    add pry-coolline for interactive coloring

--- template.rb
+++ template.rb
@@ -16,6 +16,7 @@ gem_group :development do
   gem 'erb2haml'
   gem 'hirb'         # モデルの出力結果を表形式で表示するGem
   gem 'hirb-unicode' # 日本語などマルチバイト文字の出力時の出力結果のずれに対応
+  gem 'pry-coolline'
 end
 
 run 'bundle install'
