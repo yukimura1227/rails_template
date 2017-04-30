fdca327f370c2043d6b8b7c640864956c3de6e1c
yukimura1227 <takamura1227@gmail.com>
2017-04-30 13:54:33 +0900

    fix indent

--- template.rb
+++ template.rb
@@ -83,42 +83,42 @@ insert_into_file 'spec/rails_helper.rb', after: "RSpec.configure do |config|\n" 
 end
 
 file '.pryrc', <<-FOR_AWESOMEPRINT
-  # setup for awesomeprint
-  begin
-    require 'awesome_print'
-    AwesomePrint.pry!
-  rescue LoadError
-    puts 'no awesome_print :('
-  end
+# setup for awesomeprint
+begin
+  require 'awesome_print'
+  AwesomePrint.pry!
+rescue LoadError
+  puts 'no awesome_print :('
+end
 FOR_AWESOMEPRINT
 
 append_to_file '.pryrc', <<-FOR_HIRB
-  # setup for hirb
-  begin
-    require 'hirb'
-  rescue LoadError
-    puts 'no hirb :('
-  end
+# setup for hirb
+begin
+  require 'hirb'
+rescue LoadError
+  puts 'no hirb :('
+end
 
-  if defined? Hirb
-    # Slightly dirty hack to fully support in-session Hirb.disable/enable toggling
-    Hirb::View.instance_eval do
-      def enable_output_method
-        @output_method = true
-        @old_print = Pry.config.print
-        Pry.config.print = proc do |*args|
-          Hirb::View.view_or_page_output(args[1]) || @old_print.call(*args)
-        end
-      end
-
-      def disable_output_method
-        Pry.config.print = @old_print
-        @output_method = nil
+if defined? Hirb
+  # Slightly dirty hack to fully support in-session Hirb.disable/enable toggling
+  Hirb::View.instance_eval do
+    def enable_output_method
+      @output_method = true
+      @old_print = Pry.config.print
+      Pry.config.print = proc do |*args|
+        Hirb::View.view_or_page_output(args[1]) || @old_print.call(*args)
       end
     end
 
-    Hirb.enable
+    def disable_output_method
+      Pry.config.print = @old_print
+      @output_method = nil
+    end
   end
+
+  Hirb.enable
+end
 FOR_HIRB
 
 # TODO: capybara, poltergyst, bullet, Guard, timecop, mailcatcher
