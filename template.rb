7b3899069cc413653f9f2990f1cc47051ca9dcdf
yukimura1227 <takamura1227@gmail.com>
2017-05-04 22:45:35 +0900

    add timecop

--- template.rb
+++ template.rb
@@ -34,6 +34,7 @@ gem_group :test do
   gem 'capybara'
   gem 'poltergeist'
   gem 'database_cleaner'
+  gem 'timecop'
 end
 
 run 'bundle install'
@@ -161,6 +162,30 @@ def take_screenshot
 end
 SPEC_UTILITY
 
+file 'spec/features/timecop_sample_spec.rb', <<-TIMECOP_SAMPLE_SPEC
+require 'rails_helper'
+
+feature 'timecop' do
+  context 'Timecop#freeze sample' do
+    before { Timecop.freeze(Time.now) }
+    after { Timecop.return }
+    let(:now) { Time.now }
+    scenario 'Timecop#freeze stop the Time.now' do
+      expect(Time.now).to eq now
+    end
+  end
+  context 'Timecop#travel sample' do
+    let(:travel_time) { Time.local(2035, 6, 10, 16, 22, 10) }
+    before { Timecop.travel(travel_time) }
+    after { Timecop.return }
+    scenario 'time cop travel go to the target time' do
+      expect(Time.now).to_not eq travel_time
+      expect(Time.now > travel_time).to eq true
+    end
+  end
+end
+TIMECOP_SAMPLE_SPEC
+
 file 'spec/features/blogs_spec.rb', <<-SAMPLE_FEATURE_SPEC
 require 'rails_helper'
 
@@ -235,4 +260,4 @@ if defined? Hirb
 end
 FOR_HIRB
 
-# TODO: Guard, timecop, mailcatcher
+# TODO: Guard, mailcatcher
