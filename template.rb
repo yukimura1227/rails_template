8d459f5549572cdef06e361806c9f1b449784980
Hiroyuki Takamura <takamura1227@gmail.com>
2019-10-14 15:50:19 +0900

    fix: bootstrap-railsを除去したことによってrspecがおちるようになったので改善

--- template.rb
+++ template.rb
@@ -214,7 +214,7 @@ require 'rails_helper'
 feature 'blogs' do
   scenario 'can show blogs', js: true do
     visit blogs_path
-    expect(page).to have_content 'Blogs'
+    expect(page).to have_content 'blogs'
   end
   scenario 'can using font-awesome-rails', js: true do
     visit blogs_path
@@ -225,19 +225,19 @@ feature 'blogs' do
     click_on 'New'
     fill_in 'Title', with: 'dummy title'
     fill_in 'Content', with: 'dummy content'
-    expect { click_on 'Create Blog' }.to change(Blog, :count).by(1)
+    expect { click_on 'Save' }.to change(Blog, :count).by(1)
   end
   scenario 'can create blogs twice', js: true do
     visit blogs_path
     click_on 'New'
     fill_in 'Title', with: 'dummy title'
     fill_in 'Content', with: 'dummy content'
-    expect { click_on 'Create Blog' }.to change(Blog, :count).by(1)
+    expect { click_on 'Save' }.to change(Blog, :count).by(1)
     visit blogs_path
     click_on 'New'
     fill_in 'Title', with: 'dummy title2'
     fill_in 'Content', with: 'dummy content2'
-    expect { click_on 'Create Blog' }.to change(Blog, :count).by(1)
+    expect { click_on 'Save' }.to change(Blog, :count).by(1)
     expect(Blog.count).to eq 2
   end
 end
