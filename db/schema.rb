c4eb28642db6b810dd5ea6de0cd611386bfd304e
yukimura1227 <takamura1227@gmail.com>
2020-01-04 10:37:30 +0900

    fix: docker上のmysqlに接続できるようにして、db:migrate:resetを実施

--- db/schema.rb
+++ db/schema.rb
@@ -12,19 +12,20 @@
 
 ActiveRecord::Schema.define(version: 2020_01_03_012621) do
 
-  create_table "blogs", force: :cascade do |t|
+  create_table "blogs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "comments", force: :cascade do |t|
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content"
-    t.integer "blog_id"
+    t.bigint "blog_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["blog_id"], name: "index_comments_on_blog_id"
   end
 
+  add_foreign_key "comments", "blogs"
 end
